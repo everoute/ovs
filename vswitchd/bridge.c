4e77707a2977e416c46db7a61fa5ab6e07234252
Changliang Wu <changliang.wu@smartx.com>
2025-09-16 10:08:07 +0800

    test lacp

--- vswitchd/bridge.c
+++ vswitchd/bridge.c
@@ -3343,6 +3343,11 @@ bridge_run(void)
                                         "flow-restore-wait", false));
     }
 
+    if (cfg && ofproto_get_lacp_restore_wait()) {
+        ofproto_set_lacp_restore_wait(smap_get_bool(&cfg->other_config,
+                                        "lacp-restore-wait", false));
+    }
+
     bridge_run__();
 
     /* Re-configure SSL.  We do this on every trip through the main loop,
